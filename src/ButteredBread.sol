09ab96889ce3ff924a02bb60d92d603980dce704
Hunter King <hking6776@gmail.com>
2024-08-11 15:17:24 -0500

    init

---
+++ src/ButteredBread.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import {ERC20VotesUpgradeable} from
+    "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title Breadchain Buttered Bread
+ * @notice
+ * @author Breadchain Collective
+ * @custom:coauthor @RonTuretzky
+ * @custom:coauthor @daopunk
+ */
+contract ButteredBread is ERC20VotesUpgradeable, OwnableUpgradeable {
+    error NonTransferable();
+    error InsufficientFunds();
+
+    mapping(address butter => bool allow) public allowlistedLP;
+    mapping(address account => mapping(address butter => uint256 balance)) public accountToLPBalances;
+    mapping(address account => uint256 factor) public scalingFactors;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address[] memory _liquidityPools) external initializer {
+        __Ownable_init(msg.sender);
+        for (uint256 i; i < _liquidityPools.length; ++i) {
+            allowlistedLP[_liquidityPools[i]] = true;
+        }
+    }
+
+    function balanceOfButter(address _account, address _butter) external view returns (uint256 _balance) {
+        _balance = accountToLPBalances[_account][_butter];
+    }
+
+    function deposit(address _butter, uint256 _amount) external {
+        _deposit(msg.sender, _butter, _amount);
+    }
+
+    function withdraw(address _butter, uint256 _amount) external {
+        _withdraw(msg.sender, _butter, _amount);
+    }
+
+    /// @dev ButteredBread tokens are non-transferable
+    function transfer(address, uint256) public virtual override returns (bool) {
+        revert NonTransferable();
+        return false;
+    }
+
+    /// @dev ButteredBread tokens are non-transferable
+    function transferFrom(address, address, uint256) public virtual override returns (bool) {
+        revert NonTransferable();
+        return false;
+    }
+
+    function _deposit(address _account, address _butter, uint256 _amount) internal {
+        uint256 beforeBalance = accountToLPBalances[_account][_butter];
+        accountToLPBalances[_account][_butter] = beforeBalance + _amount;
+    }
+
+    function _withdraw(address _account, address _butter, uint256 _amount) internal {
+        uint256 beforeBalance = accountToLPBalances[_account][_butter];
+        if (_amount > beforeBalance) revert InsufficientFunds();
+        accountToLPBalances[_account][_butter] = beforeBalance - _amount;
+        IERC20(_butter).transfer(_account, _amount);
+    }
+}
