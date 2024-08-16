4ab10322ddb87f1b0f2c17949ca14e59e3c66268
Hunter King <hking6776@gmail.com>
2024-08-16 00:15:24 -0500

    add comments

--- src/ButteredBread.sol
+++ src/ButteredBread.sol
@@ -136,10 +136,10 @@ contract ButteredBread is ERC20VotesUpgradeable, OwnableUpgradeable, IButteredBr
 
     /// @notice Sync voting weight with scaling factor
     function _syncVotingWeight(address _account, address _lp) internal {
-        uint256 currentScalingFactor = scalingFactors[_lp];
-        uint256 initialScalingFactor = _accountToLPData[_account][_lp].scalingFactor;
-
-        if (currentScalingFactor != initialScalingFactor) {
+        if (scalingFactors[_lp] != _accountToLPData[_account][_lp].scalingFactor) {
+            /// @dev due to false likelyhood, variables declared only if true
+            uint256 currentScalingFactor = scalingFactors[_lp];
+            uint256 initialScalingFactor = _accountToLPData[_account][_lp].scalingFactor;
             uint256 lpBalance = _accountToLPData[_account][_lp].balance;
 
             if (currentScalingFactor > initialScalingFactor) {
