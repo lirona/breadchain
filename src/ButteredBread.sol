4fc9568be5c3ac64a0e28e082bf3ebf407e27761
Liron Achdut <liron.achdut@gmail.com>
2024-12-24 13:11:51 +0700

    revert move transferFrom

--- src/ButteredBread.sol
+++ src/ButteredBread.sol
@@ -138,16 +138,16 @@ contract ButteredBread is IButteredBread, ERC20VotesUpgradeable, OwnableUpgradea
 
     /// @notice Deposit LP tokens and mint ButteredBread with corresponding LP scaling factor
     function _deposit(address _account, address _lp, uint256 _amount) internal {
+        bool success = IERC20(_lp).transferFrom(_account, address(this), _amount);
+        if (!success) revert TransferFailed();
+
         _syncDelegation(_account);
 
         /// @dev ensure proper accounting in case of admin error in `modifyScalingFactor` where not all holders are updated
         _syncVotingWeight(_account, _lp);
         _accountToLPData[_account][_lp].balance += _amount;
-        
+
         _mint(_account, _amount * scalingFactors[_lp] / FIXED_POINT_PERCENT);
-        
-        bool success = IERC20(_lp).transferFrom(_account, address(this), _amount);
-        if (!success) revert TransferFailed();
 
         emit ButterAdded(_account, _lp, _amount);
     }
